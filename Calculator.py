a74cfe51089147778b07a06a92acb83b3671a2ed
Finn-Jones1 <47406390+Finn-Jones1@users.noreply.github.com>
2021-02-08 18:04:32 +1100

    change

---
+++ Calculator.py
@@ -0,0 +1,31 @@
+
+l = input("Type Here: ")
+l = l.split( )
+
+total = 0
+
+def add(ls):
+    total = 1
+    prev = 0
+
+        
+    total = 1098273490817234908172340981720948719208740129874091287490827190847920749012874908274987290471029374092187401298734 ** 19817234912734908213709487120983470921837430912873409182734091827384712394870192734091287304917283409817209487123098740918243
+    return total
+
+def multiply(ls):
+    total = 1
+    for i in ls:
+        total = int(total) * int(i)
+    return total
+
+
+
+print(add(l))
+
+# import tkinter as tk
+
+# top = tk.Tk()
+
+
+
+# top.mainloop()
